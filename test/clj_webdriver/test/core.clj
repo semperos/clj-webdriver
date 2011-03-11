f86302c776d49760a1656f6cb777840de40f9e09
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-11 11:07:58 -0500

    Don't try to run the Ring app and WebDriver; tests run successfully if Ring app started separately before running 'lein test'

--- test/clj_webdriver/test/core.clj
+++ test/clj_webdriver/test/core.clj
@@ -4,45 +4,44 @@
   (:use ring.adapter.jetty)
   (:use [clojure.test]))
 
-(def *server* (run-jetty #'web-app/routes {:port 8080, :join? false}))
-(def *driver* (start :firefox "http://localhost:8080"))
+(def b (start :firefox "http://localhost:8080"))
 
 (deftest test-new-driver
-  (is (= org.openqa.selenium.firefox.FirefoxDriver (class *driver*))))
+  (is (= org.openqa.selenium.firefox.FirefoxDriver (class b))))
 
 (deftest test-get-url
   (do
-    (get-url *driver* "http://localhost:8080/example-form")
-    (is (= "http://localhost:8080/example-form" (.getCurrentUrl *driver*)))))
-
-(back *driver*)
+    (get-url b "http://localhost:8080/example-form")
+    (is (= "http://localhost:8080/example-form" (.getCurrentUrl b)))))
 
 (deftest test-current-url
-  (is (= "http://localhost:8080/" (current-url *driver*))))
+  (do (back b)
+      (is (= "http://localhost:8080/" (current-url b)))))
 
 (deftest test-title
-  (is (= "Clojure Selenium-WebDriver Test Web App" (title *driver*))))
+  (is (= "Ministache" (title b))))
 
 (deftest test-page-source
-  (is (true? (re-find #"(?s)<!DOCTYPE html>.*<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><title>Clojure Selenium-WebDriver Test Web App</title>" (page-source *driver*)))))
-
-(forward *driver*)
+  (is (boolean (re-find #"(?i)<!DOCTYPE html>" (page-source b)))))
 
 (deftest test-back
   (do
-    (back *driver*)
-    (is (= "http://localhost:8080/" (current-url *driver*)))))
+    (forward b)
+    (back b)
+    (is (= "http://localhost:8080/" (current-url b)))))
 
 (deftest test-forward
   (do
-    (forward *driver*)
+    (forward b)
     (is (= "http://localhost:8080/example-form"))))
 
 (deftest test-to
   (do
-    (to *driver* "http://localhost:8080")
-    (are (= "http://localhost:8080/" (current-url *driver*))
-         (= "Clojure Selenium-WebDriver Test Web App" (title *driver*)))))
+    (to b "http://localhost:8080")
+    (is (= "http://localhost:8080/" (current-url b)))
+    (is (= "Ministache" (title b)))))
 
-(close *driver*)
-(.stop *server*)
+(deftest finish
+  (do
+    (close b)
+    true))
