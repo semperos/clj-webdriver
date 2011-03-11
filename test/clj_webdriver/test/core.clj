cfea4e4c9c8049b51f02ab46c8c7d300a1d263ef
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-11 04:39:55 -0500

    Tests that don't pass

--- test/clj_webdriver/test/core.clj
+++ test/clj_webdriver/test/core.clj
@@ -1,6 +1,48 @@
-(ns selenium-webdriver-clj.test.core
-  (:use [selenium-webdriver-clj.core] :reload)
+(ns clj-webdriver.test.core
+  (:require [clj-webdriver.test.example-app.core :as web-app])
+  (:use [clj-webdriver.core] :reload)
+  (:use ring.adapter.jetty)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(def *server* (run-jetty #'web-app/routes {:port 8080, :join? false}))
+(def *driver* (start :firefox "http://localhost:8080"))
+
+(deftest test-new-driver
+  (is (= org.openqa.selenium.firefox.FirefoxDriver (class *driver*))))
+
+(deftest test-get-url
+  (do
+    (get-url *driver* "http://localhost:8080/example-form")
+    (is (= "http://localhost:8080/example-form" (.getCurrentUrl *driver*)))))
+
+(back *driver*)
+
+(deftest test-current-url
+  (is (= "http://localhost:8080/" (current-url *driver*))))
+
+(deftest test-title
+  (is (= "Clojure Selenium-WebDriver Test Web App" (title *driver*))))
+
+(deftest test-page-source
+  (is (true? (re-find #"(?s)<!DOCTYPE html>.*<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><title>Clojure Selenium-WebDriver Test Web App</title>" (page-source *driver*)))))
+
+(forward *driver*)
+
+(deftest test-back
+  (do
+    (back *driver*)
+    (is (= "http://localhost:8080/" (current-url *driver*)))))
+
+(deftest test-forward
+  (do
+    (forward *driver*)
+    (is (= "http://localhost:8080/example-form"))))
+
+(deftest test-to
+  (do
+    (to *driver* "http://localhost:8080")
+    (are (= "http://localhost:8080/" (current-url *driver*))
+         (= "Clojure Selenium-WebDriver Test Web App" (title *driver*)))))
+
+(close *driver*)
+(.stop *server*)
