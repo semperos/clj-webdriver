4832a451d787956ff8ea6b3d706dd5b258836cb2
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-15 17:56:03 -0400

    Add comments for new firefox fns

--- src/clj_webdriver/firefox.clj
+++ src/clj_webdriver/firefox.clj
@@ -10,21 +10,23 @@
   (FirefoxProfile.))
 
 (defn enable-extension
-  "Given a `FirefoxProfile` object, enable an extension located at `extension-location`"
+  "Given a `FirefoxProfile` object, enable an extension located at `extension-location`."
   [profile extension-location]
   (let [ext-file (io/as-file extension-location)]
     (.addExtension profile ext-file)))
 
 (defn set-preferences
-  "Given a `FirefoxProfile` object and a map of preferences, set the preferences for the profile"
+  "Given a `FirefoxProfile` object and a map of preferences, set the preferences for the profile."
   [profile pref-m]
   (doseq [entry pref-m]
     (.setPreference profile (name (key entry)) (val entry))))
 
 (defn accept-untrusted-certs
+  "Set whether or not Firefox should accept untrusted certificates."
   [profile ^Boolean bool]
   (.setAcceptUntrustedCertificates profile bool))
 
 (defn enable-native-events
+  "Set whether or not native events should be enabled (true by default on Windows, false on other platforms)."
   [profile ^Boolean bool]
   (.setEnableNativeEvents profile bool))
