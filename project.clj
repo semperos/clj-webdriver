0cfdd925192b8a5b8bfc9894e10f213e07d8e88d
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-03-18 16:32:08 -0400

    Add ninjudd's ordered-set to dependencies, so that we can properly handle .getWindowHandles, which returns a Java LinkedHashSet

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-webdriver "0.1.0"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.2.0"]
+                 [ordered-set "0.1.0"]
 		 [org.seleniumhq.selenium/selenium-server "2.0b2"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
 		     [marginalia "0.5.0"]
