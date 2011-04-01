6454c9d31296c2fdd8a11c82427d583072287982
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-01 15:01:19 -0400

    Update tests to stop using find-it fn

--- test/clj_webdriver/test/core.clj
+++ test/clj_webdriver/test/core.clj
@@ -29,7 +29,8 @@
 
 (deftest test-back-forward
   (-> b
-      (find-it :a {:text "example form"})
+      (find-them :a {:text "example form"})
+      first
       click)
   (is (= "http://localhost:8080/example-form" (current-url b)))
   (back b)
@@ -44,7 +45,8 @@
 
 (deftest test-bys
   (-> b
-      (find-it :a {:text "example form"})
+      (find-them :a {:text "example form"})
+      first
       click)
   (is (= "first_name"
          (attribute (find-element b (by-id "first_name")) :id)))
@@ -65,27 +67,27 @@
 
 (deftest test-find*
   (is (= "Moustache"
-         (text (find-it b :a))))
+         (text (first (find-them b :a)))))
   (is (= "Moustache"
-         (text (find-it b {:class "external"}))))
+         (text (first (find-them b {:class "external"})))))
   (is (= "https://github.com/cgrand/moustache"
-         (attribute (find-it b {:text "Moustache"}) "href")))
+         (attribute (first (find-them b {:text "Moustache"})) "href")))
   (is (= "Moustache"
-         (text (find-it b :a {:class #"exter"}))))
+         (text (first (find-them b :a {:class #"exter"})))))
   (is (= "Moustache"
-         (text (find-it b :a {:text #"Mous"}))))
+         (text (first (find-them b :a {:text #"Mous"})))))
   (is (= "Moustache"
-         (text (find-it b :a {:class "external", :href #"github"}))))
+         (text (first (find-them b :a {:class "external", :href #"github"})))))
   (is (= "Moustache"
-         (text (find-it b :a {:class #"exter", :href #"github"}))))
+         (text (first (find-them b :a {:class #"exter", :href #"github"})))))
   (is (= "Moustache"
-         (text (find-it b [:div {:id "content"}, :a {:class "external"}]))))
+         (text (first (find-them b [:div {:id "content"}, :a {:class "external"}])))))
   (is (= "Moustache"
-         (text (find-it b [:div {:id "content"}, :a {:class #"exter"}]))))
+         (text (first (find-them b [:div {:id "content"}, :a {:class #"exter"}])))))
   (is (= "Moustache"
-         (text (find-it b [:div {:id "content"}, :a {:href #"github"}]))))
+         (text (first (find-them b [:div {:id "content"}, :a {:href #"github"}])))))
   (is (= "home"
-         (text (find-it b [:* {:id "footer"}, :a {}]))))
+         (text (first (find-them b [:* {:id "footer"}, :a {}])))))
   (is (= 6
          (count (find-them b :a))))
   (is (= 3
@@ -103,78 +105,87 @@
   (is (= 2
          (count (find-them b [:div {:id "content"}, :a {:class #"exter"}]))))
   (-> b
-      (find-it :a {:text "example form"})
+      (find-them :a {:text "example form"})
+      first
       click)
   (is (= "first_name"
-         (attribute (find-it b {:type "text"}) "id")))
+         (attribute (first (find-them b {:type "text"})) "id")))
   (is (= "first_name"
-         (attribute (find-it b :input {:type "text"}) "id")))
+         (attribute (first (find-them b :input {:type "text"})) "id")))
   (is (= "first_name"
-         (attribute (find-it b :input {:type "text", :name "first_name"}) "id")))
+         (attribute (first (find-them b :input {:type "text", :name "first_name"})) "id")))
   (is (= "first_name"
-         (attribute (find-it b :input {:type "text", :name #"first_"}) "id")))
+         (attribute (first (find-them b :input {:type "text", :name #"first_"})) "id")))
   (is (= "last_name"
-         (attribute (find-it b :input {:type "text", :name #"last_"}) "id")))
+         (attribute (first (find-them b :input {:type "text", :name #"last_"})) "id")))
   (is (= "Smith"
-         (attribute (find-it b :input {:type "text", :name #"last_"}) "value")))
+         (attribute (first (find-them b :input {:type "text", :name #"last_"})) "value")))
   (is (= "Smith"
-         (attribute (find-it b :input {:type "text", :name #"last_"}) "value")))
+         (attribute (first (find-them b :input {:type "text", :name #"last_"})) "value")))
   (is (= "Smith"
-         (attribute (find-it b [:div {:id "content"}, :input {:name #"last_"}]) "value")))
+         (attribute (first (find-them b [:div {:id "content"}, :input {:name #"last_"}])) "value")))
   (is (true?
        (-> b
-           (find-it :a)
+           (find-them :a)
+           first
            exists?)))
   (is (false?
        (-> b
-           (find-it :area)
+           (find-them :area)
+           first
            exists?))))
 
 (deftest test-form-elements
   (to b "http://localhost:8080/example-form")
   ;; Clear element
   (-> b
-      (find-it [:form {:id "example_form"}, :input {:name #"last_"}])
+      (find-them [:form {:id "example_form"}, :input {:name #"last_"}])
+      first
       clear)
   (is (= ""
-         (value (find-it b [:form {:id "example_form"}, :input {:name #"last_"}]))))
+         (value (first (find-them b [:form {:id "example_form"}, :input {:name #"last_"}])))))
   ;; Radio buttons
   (is (= true
-         (selected? (find-it b :input {:type "radio", :value "male"}))))
+         (selected? (first (find-them b :input {:type "radio", :value "male"})))))
   (-> b
-      (find-it :input {:type "radio", :value "female"})
+      (find-them :input {:type "radio", :value "female"})
+      first
       select)
   (is (= true
-         (selected? (find-it b :input {:type "radio", :value "female"}))))
+         (selected? (first (find-them b :input {:type "radio", :value "female"})))))
   (is (= false
-         (selected? (find-it b :input {:type "radio", :value "male"}))))
+         (selected? (first (find-them b :input {:type "radio", :value "male"})))))
   ;; Checkboxes
   (is (= false
-         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
+         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
   (-> b
-      (find-it :input {:type "checkbox", :name #"(?i)clojure"})
+      (find-them :input {:type "checkbox", :name #"(?i)clojure"})
+      first
       toggle)
   (is (= true
-         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
+         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
   (-> b
-      (find-it :input {:type "checkbox", :name #"(?i)clojure"})
+      (find-them :input {:type "checkbox", :name #"(?i)clojure"})
+      first
       click)
   (is (= false
-         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
+         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
   (-> b
-      (find-it :input {:type "checkbox", :name #"(?i)clojure"})
+      (find-them :input {:type "checkbox", :name #"(?i)clojure"})
+      first
       select)
   (is (= true
-         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
+         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
   ;; Text fields
   (is (= "true"
-         (attribute (find-it b :input {:type "text", :value "Testing!"})
+         (attribute (first (find-them b :input {:type "text", :value "Testing!"}))
                     "readonly")))
   (-> b
-      (find-it :input {:id "first_name"})
+      (find-them :input {:id "first_name"})
+      first
       (input-text "foobar"))
   (is (= "foobar"
-         (value (find-it b :input {:id "first_name"})))))
+         (value (first (find-them b :input {:id "first_name"}))))))
 
 (deftest test-window-handling
   (is (= 1
@@ -182,7 +193,8 @@
   (is (= "Ministache"
          (:title (window-handle b))))
   (-> b
-      (find-it :a {:text "is amazing!"})
+      (find-them :a {:text "is amazing!"})
+      first
       click)
   (is (= "Ministache"
          (:title (window-handle b))))
@@ -195,7 +207,8 @@
   (is (= "http://localhost:8080/"
          (:url (window-handle b))))
   (-> b
-      (find-it :window {:url "http://localhost:8080/clojure"})
+      (find-them :window {:url "http://localhost:8080/clojure"})
+      first
       switch-to-window)
   (close b)
   (is (= "http://localhost:8080/"
