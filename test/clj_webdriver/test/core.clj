59a8e3c4a6ca26407903b1d9e8a3078f9e9914af
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-01 15:08:39 -0400

    Undo changes to test cases; use find-it again (see previous commit about how find-it has changed

--- test/clj_webdriver/test/core.clj
+++ test/clj_webdriver/test/core.clj
@@ -29,8 +29,7 @@
 
 (deftest test-back-forward
   (-> b
-      (find-them :a {:text "example form"})
-      first
+      (find-it :a {:text "example form"})
       click)
   (is (= "http://localhost:8080/example-form" (current-url b)))
   (back b)
@@ -45,8 +44,7 @@
 
 (deftest test-bys
   (-> b
-      (find-them :a {:text "example form"})
-      first
+      (find-it :a {:text "example form"})
       click)
   (is (= "first_name"
          (attribute (find-element b (by-id "first_name")) :id)))
@@ -67,27 +65,27 @@
 
 (deftest test-find*
   (is (= "Moustache"
-         (text (first (find-them b :a)))))
+         (text (find-it b :a))))
   (is (= "Moustache"
-         (text (first (find-them b {:class "external"})))))
+         (text (find-it b {:class "external"}))))
   (is (= "https://github.com/cgrand/moustache"
-         (attribute (first (find-them b {:text "Moustache"})) "href")))
+         (attribute (find-it b {:text "Moustache"}) "href")))
   (is (= "Moustache"
-         (text (first (find-them b :a {:class #"exter"})))))
+         (text (find-it b :a {:class #"exter"}))))
   (is (= "Moustache"
-         (text (first (find-them b :a {:text #"Mous"})))))
+         (text (find-it b :a {:text #"Mous"}))))
   (is (= "Moustache"
-         (text (first (find-them b :a {:class "external", :href #"github"})))))
+         (text (find-it b :a {:class "external", :href #"github"}))))
   (is (= "Moustache"
-         (text (first (find-them b :a {:class #"exter", :href #"github"})))))
+         (text (find-it b :a {:class #"exter", :href #"github"}))))
   (is (= "Moustache"
-         (text (first (find-them b [:div {:id "content"}, :a {:class "external"}])))))
+         (text (find-it b [:div {:id "content"}, :a {:class "external"}]))))
   (is (= "Moustache"
-         (text (first (find-them b [:div {:id "content"}, :a {:class #"exter"}])))))
+         (text (find-it b [:div {:id "content"}, :a {:class #"exter"}]))))
   (is (= "Moustache"
-         (text (first (find-them b [:div {:id "content"}, :a {:href #"github"}])))))
+         (text (find-it b [:div {:id "content"}, :a {:href #"github"}]))))
   (is (= "home"
-         (text (first (find-them b [:* {:id "footer"}, :a {}])))))
+         (text (find-it b [:* {:id "footer"}, :a {}]))))
   (is (= 6
          (count (find-them b :a))))
   (is (= 3
@@ -105,87 +103,78 @@
   (is (= 2
          (count (find-them b [:div {:id "content"}, :a {:class #"exter"}]))))
   (-> b
-      (find-them :a {:text "example form"})
-      first
+      (find-it :a {:text "example form"})
       click)
   (is (= "first_name"
-         (attribute (first (find-them b {:type "text"})) "id")))
+         (attribute (find-it b {:type "text"}) "id")))
   (is (= "first_name"
-         (attribute (first (find-them b :input {:type "text"})) "id")))
+         (attribute (find-it b :input {:type "text"}) "id")))
   (is (= "first_name"
-         (attribute (first (find-them b :input {:type "text", :name "first_name"})) "id")))
+         (attribute (find-it b :input {:type "text", :name "first_name"}) "id")))
   (is (= "first_name"
-         (attribute (first (find-them b :input {:type "text", :name #"first_"})) "id")))
+         (attribute (find-it b :input {:type "text", :name #"first_"}) "id")))
   (is (= "last_name"
-         (attribute (first (find-them b :input {:type "text", :name #"last_"})) "id")))
+         (attribute (find-it b :input {:type "text", :name #"last_"}) "id")))
   (is (= "Smith"
-         (attribute (first (find-them b :input {:type "text", :name #"last_"})) "value")))
+         (attribute (find-it b :input {:type "text", :name #"last_"}) "value")))
   (is (= "Smith"
-         (attribute (first (find-them b :input {:type "text", :name #"last_"})) "value")))
+         (attribute (find-it b :input {:type "text", :name #"last_"}) "value")))
   (is (= "Smith"
-         (attribute (first (find-them b [:div {:id "content"}, :input {:name #"last_"}])) "value")))
+         (attribute (find-it b [:div {:id "content"}, :input {:name #"last_"}]) "value")))
   (is (true?
        (-> b
-           (find-them :a)
-           first
+           (find-it :a)
            exists?)))
   (is (false?
        (-> b
-           (find-them :area)
-           first
+           (find-it :area)
            exists?))))
 
 (deftest test-form-elements
   (to b "http://localhost:8080/example-form")
   ;; Clear element
   (-> b
-      (find-them [:form {:id "example_form"}, :input {:name #"last_"}])
-      first
+      (find-it [:form {:id "example_form"}, :input {:name #"last_"}])
       clear)
   (is (= ""
-         (value (first (find-them b [:form {:id "example_form"}, :input {:name #"last_"}])))))
+         (value (find-it b [:form {:id "example_form"}, :input {:name #"last_"}]))))
   ;; Radio buttons
   (is (= true
-         (selected? (first (find-them b :input {:type "radio", :value "male"})))))
+         (selected? (find-it b :input {:type "radio", :value "male"}))))
   (-> b
-      (find-them :input {:type "radio", :value "female"})
-      first
+      (find-it :input {:type "radio", :value "female"})
       select)
   (is (= true
-         (selected? (first (find-them b :input {:type "radio", :value "female"})))))
+         (selected? (find-it b :input {:type "radio", :value "female"}))))
   (is (= false
-         (selected? (first (find-them b :input {:type "radio", :value "male"})))))
+         (selected? (find-it b :input {:type "radio", :value "male"}))))
   ;; Checkboxes
   (is (= false
-         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
+         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
   (-> b
-      (find-them :input {:type "checkbox", :name #"(?i)clojure"})
-      first
+      (find-it :input {:type "checkbox", :name #"(?i)clojure"})
       toggle)
   (is (= true
-         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
+         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
   (-> b
-      (find-them :input {:type "checkbox", :name #"(?i)clojure"})
-      first
+      (find-it :input {:type "checkbox", :name #"(?i)clojure"})
       click)
   (is (= false
-         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
+         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
   (-> b
-      (find-them :input {:type "checkbox", :name #"(?i)clojure"})
-      first
+      (find-it :input {:type "checkbox", :name #"(?i)clojure"})
       select)
   (is (= true
-         (selected? (first (find-them b :input {:type "checkbox", :name #"(?i)clojure"})))))
+         (selected? (find-it b :input {:type "checkbox", :name #"(?i)clojure"}))))
   ;; Text fields
   (is (= "true"
-         (attribute (first (find-them b :input {:type "text", :value "Testing!"}))
+         (attribute (find-it b :input {:type "text", :value "Testing!"})
                     "readonly")))
   (-> b
-      (find-them :input {:id "first_name"})
-      first
+      (find-it :input {:id "first_name"})
       (input-text "foobar"))
   (is (= "foobar"
-         (value (first (find-them b :input {:id "first_name"}))))))
+         (value (find-it b :input {:id "first_name"})))))
 
 (deftest test-window-handling
   (is (= 1
@@ -193,8 +182,7 @@
   (is (= "Ministache"
          (:title (window-handle b))))
   (-> b
-      (find-them :a {:text "is amazing!"})
-      first
+      (find-it :a {:text "is amazing!"})
       click)
   (is (= "Ministache"
          (:title (window-handle b))))
@@ -207,8 +195,7 @@
   (is (= "http://localhost:8080/"
          (:url (window-handle b))))
   (-> b
-      (find-them :window {:url "http://localhost:8080/clojure"})
-      first
+      (find-it :window {:url "http://localhost:8080/clojure"})
       switch-to-window)
   (close b)
   (is (= "http://localhost:8080/"
