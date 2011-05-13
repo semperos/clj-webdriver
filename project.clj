d54ebbdc7f46075e76f857bc942de5e5a15f717d
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-05-13 12:49:16 -0400

    Bump Clojure version to 1.2.1, most recent stable

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-webdriver "0.2.6"
   :description "Clojure wrapper around Selenium-WebDriver library"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.seleniumhq.selenium/selenium-server "2.0b3"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
 		     [marginalia "0.5.0"]
