d744c7449af25e9468d1b10dd9f4df986fd00ef6
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-08-07 14:00:34 -0400

    Use Selenium-WebDriver 2.3.0 (latest stable), add java.net maven repo for JNA dep, and bump project version to 0.2.8

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clj-webdriver "0.2.7"
+(defproject clj-webdriver "0.2.8"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.2.1"]
-		 [org.seleniumhq.selenium/selenium-server "2.0b3"]]
+		 [org.seleniumhq.selenium/selenium-server "2.3.0"]]
   :dev-dependencies [[marginalia "0.6.0"]
                      [ring "0.3.7"]
                      [enlive "1.0.0"]
                      [net.cgrand/moustache "1.0.0"]]
-  :repositories {"selenium-repository", "http://selenium.googlecode.com/svn/repository/"})
+  :repositories {"selenium-repository", "http://selenium.googlecode.com/svn/repository/"
+                 "java-dot-net", "http://download.java.net/maven/2"})
