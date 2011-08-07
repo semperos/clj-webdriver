19fe95d0ccddc10a56854d9b118ed8ce4ab03b7e
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-08-06 21:31:46 -0400

    Remove swank-clojure dependency (let folks use plugin) and update Marginalia version

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.seleniumhq.selenium/selenium-server "2.0b3"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-		     [marginalia "0.5.0"]
+  :dev-dependencies [[marginalia "0.6.0"]
                      [ring "0.3.7"]
                      [enlive "1.0.0"]
                      [net.cgrand/moustache "1.0.0"]]
