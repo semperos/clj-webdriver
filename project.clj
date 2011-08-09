f951d24e737f58bd3e60acb59cf62d753cc63eea
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-08-09 11:29:29 -0400

    Remove marginalia dev dependency; use leiningen plugin instead

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.seleniumhq.selenium/selenium-server "2.3.0"]]
-  :dev-dependencies [[marginalia "0.6.0"]
-                     [ring "0.3.7"]
+  :dev-dependencies [[ring "0.3.7"]
                      [enlive "1.0.0"]
                      [net.cgrand/moustache "1.0.0"]]
   :repositories {"selenium-repository", "http://selenium.googlecode.com/svn/repository/"
