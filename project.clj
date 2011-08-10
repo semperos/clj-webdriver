71e313d0c9b83594e8224d3077a17bcea101b88d
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-08-10 10:05:59 -0400

    Update Clojure version to 1.3.0-beta1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-webdriver "0.2.8"
   :description "Clojure wrapper around Selenium-WebDriver library"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-		 [org.seleniumhq.selenium/selenium-server "2.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+                 [org.seleniumhq.selenium/selenium-server "2.3.0"]]
   :dev-dependencies [[ring "0.3.7"]
                      [enlive "1.0.0"]
                      [net.cgrand/moustache "1.0.0"]]
