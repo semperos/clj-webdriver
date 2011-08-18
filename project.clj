c5afd301a7a812ae16be27f46352907acdd3c437
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-08-18 13:42:01 -0400

    Upgrade selenium-server dependency to latest stable, 2.4.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-webdriver "0.2.9"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [org.seleniumhq.selenium/selenium-server "2.3.0"]]
+                 [org.seleniumhq.selenium/selenium-server "2.4.0"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
                      [enlive "1.0.0"]
