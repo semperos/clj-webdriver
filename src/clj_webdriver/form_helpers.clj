fe02a1399c6772946621bf47260d749d1eb54174
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-08-18 12:48:05 -0400

    Working quick-fill function for filling out forms

--- src/clj_webdriver/form_helpers.clj
+++ src/clj_webdriver/form_helpers.clj
@@ -1,13 +1,25 @@
-;;; ## Form Helpers ##
-;;;
-;;; The functions in this namespace are designed to make working with HTML forms
-;;; faster and more intuitive for "common" use-cases.
-;;;
-(ns clj-webdriver.form-helpers)
+;; ## Form Helpers ##
+;;
+;; The functions in this namespace are designed to make working with HTML forms
+;; faster and more intuitive for "common" use-cases.
+;;
+(ns clj-webdriver.form-helpers
+  (:use [clj-webdriver.core :only [find-them]]))
 
 (defn quick-fill
-  ([fields-to-vals] (quick-fill fields-to-vals {:auto-submit true}))
-  ([fields-to-vals opts]
-     ;; run through fields-to-vals entries
-     ;; do intuitive things for field type (type into a text field, click a radio/checkbox, select a select dropdown, etc
-     ))
+  "driver              - browser driver
+   query-action-maps   - a seq of maps of queries to actions (queries find HTML elements, actions are fn's that act on them)
+   opts                - extra options"
+  ([driver query-action-maps] (quick-fill driver query-action-maps {:auto-submit true, :form-id nil}))
+  ([driver query-action-maps opts]
+     (for [entries query-action-maps,
+           [k v] entries]
+       ;; provide shortcut, if `k` is a string,
+       ;; then assume it refers to an element's id
+       (let [query-action-map (if (string? k)
+                                {:id k}
+                                k)
+             target-els (find-them driver query-action-map)]
+         (apply v target-els)))))
+
+;; also support shortcut for v as a string, meaning input-text
