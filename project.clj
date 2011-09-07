7e11783276547c5be5d59e8df2de3d5892b3c845
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-09-07 11:40:02 -0400

    Update Selenium-WebDriver dependency to 2.5.0, see its changelog here: http://code.google.com/p/selenium/source/browse/trunk/java/CHANGELOG

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-webdriver "0.2.12"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0-beta3"]
-                 [org.seleniumhq.selenium/selenium-server "2.4.0"]]
+                 [org.seleniumhq.selenium/selenium-server "2.5.0"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
                      [enlive "1.0.0"]
