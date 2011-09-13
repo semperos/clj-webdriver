0e560cf04c475a1ffaf0794196eee72f8e72ce05
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-09-13 11:38:52 -0400

    Bump Clojure to version 1.3.0-RC0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-webdriver "0.2.13"
   :description "Clojure wrapper around Selenium-WebDriver library"
-  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
                  [org.seleniumhq.selenium/selenium-server "2.5.0"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
