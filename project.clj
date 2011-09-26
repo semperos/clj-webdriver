479efdbc545312516e1b47a029f0d57309728131
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-09-26 12:22:42 -0400

    Up version of Clojure to the (now official release) of 1.3.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-webdriver "0.2.14"
   :description "Clojure wrapper around Selenium-WebDriver library"
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.seleniumhq.selenium/selenium-server "2.5.0"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
