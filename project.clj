0803088dc83202e26efffb219ec872857a7b0fb0
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-10-04 09:36:57 -0400

    Bump to version 0.2.16, fix to flash function

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-webdriver "0.2.15"
+(defproject clj-webdriver "0.2.16"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.seleniumhq.selenium/selenium-server "2.5.0"]]
