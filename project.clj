9bc182622bcad15449e402dbc95352e67cf42418
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-10-05 18:14:00 -0400

    Bump version to 0.3.0, with Grid support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-webdriver "0.2.16"
+(defproject clj-webdriver "0.3.0"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.seleniumhq.selenium/selenium-server "2.7.0"]]
