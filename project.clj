29c60f5a21689cc29260206ba45d5cb6231a7dcd
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-10-27 09:09:36 -0400

    Temporarily change clache dependency to use our own release, pending updated jar for primary repo

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clj-webdriver "0.3.2"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.seleniumhq.selenium/selenium-server "2.7.0"]]
+                 [org.seleniumhq.selenium/selenium-server "2.7.0"]
+                 [org.clojars.semperos/clache "0.6.1.1"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
                      [enlive "1.0.0"]
