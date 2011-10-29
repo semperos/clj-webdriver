ae8f1018dc27e24b0c346e865f1df31b89d6ea4c
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-10-29 11:43:02 -0400

    Add dep for logging, ignore log4j properties files

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.seleniumhq.selenium/selenium-server "2.7.0"]
-                 [clache "0.7.0"]]
+                 [clache "0.7.0"]
+                 [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
                      [enlive "1.0.0"]
