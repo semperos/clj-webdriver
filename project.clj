74a06e5255b63662f7c6c90fa92a0da7443b2ffc
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-11-04 13:34:55 -0400

    Prepare for next patch-level release, 0.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-webdriver "0.4.3"
+(defproject clj-webdriver "0.4.4-SNAPSHOT"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.seleniumhq.selenium/selenium-server "2.11.0"]
