56c68fb7964d9a257faaf9ab616966e6ddc68371
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-12-19 09:16:35 -0500

    Change dependency from fogus.clache to core.cache (thanks @fogus)

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.seleniumhq.selenium/selenium-server "2.11.0"]
-                 [clache "0.7.1"]
+                 [org.clojure/core.cache "0.5.0"]
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
