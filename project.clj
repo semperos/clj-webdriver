d5a5ccfd44bc1f71430a5320bf58bc340b45eeb3
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-01-05 00:41:36 -0500

    Bump Selenium-WebDriver to 2.16.0 now that jars are in central, remove unnecessary :repositories declarations

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject clj-webdriver "0.5.0-SNAPSHOT"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.seleniumhq.selenium/selenium-server "2.15.0"]
+                 [org.seleniumhq.selenium/selenium-server "2.16.0"]
                  [org.clojure/core.cache "0.5.0"]
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[marginalia "0.3.2"]
                      [ring "0.3.7"]
                      [enlive "1.0.0"]
-                     [net.cgrand/moustache "1.0.0"]]
-  :repositories {"selenium-repository" "http://selenium.googlecode.com/svn/repository/",
-                 "java-dot-net" "http://download.java.net/maven/2"})
+                     [net.cgrand/moustache "1.0.0"]])
