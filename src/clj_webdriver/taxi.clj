2d1e564609d0063f55f110bc89d83e79c0388016
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-02-16 01:03:31 -0500

    Improve abstraction around finder-fn, merge lower-level and taxi-level usage where possible

--- src/clj_webdriver/taxi.clj
+++ src/clj_webdriver/taxi.clj
@@ -1,12 +1,13 @@
 ;; The faster way to use clj-webdriver: take a taxi
 (ns clj-webdriver.taxi
+  (:use [clj-webdriver.element :only [is-element?]])
   (:require [clj-webdriver.core :as core]
             [clj-webdriver.options :as options])
   (:import clj_webdriver.element.Element))
 
-(declare css-finder-fn)
+(declare css-finder)
 (def ^:dynamic *driver*)
-(def ^:dynamic *finder-fn* css-finder-fn)
+(def ^:dynamic *finder-fn* css-finder)
 
 (defn- set-driver*
   [browser-spec]
@@ -33,7 +34,7 @@
 (defmacro with-driver
   "Equivalent to core's function with the same name, except no binding form needed. Browser is started at beginning of `body` and `(quit)` is called at the end."
   [browser-spec & body]
-  `(binding [*driver* (atom (core/new-driver ~browser-spec))]
+  `(binding [*driver* (core/new-driver ~browser-spec)]
     (try
       ~@body
       (finally
@@ -49,21 +50,34 @@
       (finally
         (quit)))))
 
-(defn css-finder-fn
-  "Given a CSS query `q`, return the first element found. If `q` is not a string, it's assumed to be an Element record and is returned unchanged."
+(defn css-finder
+  "Given a CSS query `q`, return a lazy seq of the elements found. If `q` is not a string, it's assumed to be an Element record and is returned unchanged."
   [q]
-  (if (not (string? q))
+  (if-not (string? q)
     q
-    (core/find-element *driver* {:css q})))
+    (core/find-elements *driver* {:css q})))
 
-(set-finder-fn! css-finder-fn)
+(set-finder-fn! css-finder)
 
-(defn xpath-finder-fn
-  "Given a `driver` and a XPath query `q`, return the first element found. If `q` is not a string, it's assumed to be an Element record and is returned unchanged."
+(defn xpath-finder
+  "Given a `driver` and a XPath query `q`, return a lazy seq of the elements found. If `q` is not a string, it's assumed to be an Element record and is returned unchanged."
   [q]
-  (if (not (string? q))
+  (if-not (string? q)
     q
-    (core/find-element *driver* {:xpath q})))
+    (core/find-elements *driver* {:xpath q})))
+
+;; Be able to get actual element/elements when needed
+(defn element
+  [q]
+  (if-not (string? q)
+    q
+    (first (*finder-fn* q))))
+
+(defn elements
+  [q]
+  (if-not (string? q)
+    q
+    (*finder-fn* q)))
 
 ;; Driver functions
 (defn to [url] (core/to *driver* url))
@@ -94,8 +108,8 @@
 (defn delete-all-cookies [] (options/delete-all-cookies *driver*))
 (defn cookies [] (options/cookies *driver*))
 (defn cookie-named [cookie-name] (options/cookie-named *driver* cookie-name))
-(defn find-element-by [by] (core/find-element-by *driver* by))
-(defn find-elements-by [by] (core/find-elements-by *driver* by))
+(defn find-element-by [by-clause] (core/find-element-by *driver* by-clause))
+(defn find-elements-by [by-clause] (core/find-elements-by *driver* by-clause))
 (defn find-elements-by-regex-alone [tag attr-val] (core/find-elements-by-regex-alone *driver* tag attr-val))
 (defn find-elements-by-regex [tag attr-val] (core/find-elements-by-regex *driver* tag attr-val))
 (defn find-windows [attr-val] (core/find-windows *driver* attr-val))
@@ -103,11 +117,29 @@
 (defn find-semantic-buttons [attr-val] (core/find-semantic-buttons *driver* attr-val))
 (defn find-semantic-buttons-by-regex [attr-val] (core/find-semantic-buttons-by-regex *driver* attr-val))
 (defn find-checkables-by-text [attr-val] (core/find-checkables-by-text *driver* attr-val))
-(defn find-table-cell [table-q coords] (core/find-table-cell *driver* (*finder-fn* table-q) coords))
-(defn find-table-row [table-q row] (core/find-table-row *driver* (*finder-fn* table-q) row))
+(defn find-table-cell [table-q coords] (core/find-table-cell *driver* (element table-q) coords))
+(defn find-table-row [table-q row] (core/find-table-row *driver* (element table-q) row))
 (defn find-by-hierarchy [hierarchy-vec] (core/find-by-hierarchy *driver* hierarchy-vec))
+
+;; Think hard on these ones
 (defn find-elements [attr-val] (core/find-elements *driver* attr-val))
 (defn find-element [attr-val] (core/find-element *driver* attr-val))
+
+;; Element versions of find-element-by and find-elements-by
+;; These will be regularized once #42 (decoupling by-* functionality) is fixed
+(defn find-element-under
+  "If q-parent isn't a string, it's assumed to be an Element"
+  [q-parent by-clause]
+  (if-not (string? q-parent)
+    (core/find-element q-parent by-clause)
+    (core/find-element (element q-parent) by-clause)))
+
+(defn find-elements-under
+  "If q-parent isn't a string, it's assumed to be an Element"
+  [q-parent by-clause]
+  (if-not (string? q-parent)
+    (core/find-elements q-parent by-clause)
+    (core/find-elements (element q-parent) by-clause)))
 
 ;; Element functions
 ;;
@@ -119,47 +151,47 @@
 ;; functions like their core counterparts, in which case each function will default
 ;; back to core functionality (expecting that you're passing in an Element record)
 
-(defn attribute [q attr] (core/attribute (*finder-fn* q) attr))
-(defn click [q] (core/click (*finder-fn* q)))
-(defn displayed? [q] (core/displayed? (*finder-fn* q)))
-(defn drag-and-drop-by [q x y] (core/drag-and-drop-by (*finder-fn* q) x y))
-(defn drag-and-drop-on [qa qb] (core/drag-and-drop-on (*finder-fn* qa) (*finder-fn* qb)))
-(defn exists? [q] (core/exists? (*finder-fn* q)))
-(defn flash [q] (core/flash (*finder-fn* q)))
-(defn focus [q] (core/focus (*finder-fn* q)))
-(defn html [q] (core/html (*finder-fn* q)))
-(defn location [q] (core/location (*finder-fn* q)))
-(defn location-once-visible [q] (core/location-once-visible (*finder-fn* q)))
-(defn present? [q] (core/present? (*finder-fn* q)))
-(defn size [q] (core/size (*finder-fn* q)))
-(defn rectangle [q] (core/rectangle (*finder-fn* q)))
-(defn intersect? [q & qs] (let [part (partial core/intersect? (*finder-fn* q))]
-                            (apply part (map *finder-fn* qs))))
-(defn tag [q] (core/tag (*finder-fn* q)))
-(defn text [q] (core/text (*finder-fn* q)))
-(defn value [q] (core/value (*finder-fn* q)))
-(defn visible? [q] (core/visible? (*finder-fn* q)))
-(defn xpath [q] (core/xpath (*finder-fn* q)))
-(defn deselect [q] (core/deselect (*finder-fn* q)))
-(defn enabled? [q] (core/enabled? (*finder-fn* q)))
-(defn input-text [q s] (core/input-text (*finder-fn* q) s))
-(defn submit [q] (core/submit (*finder-fn* q)))
-(defn clear [q] (core/clear (*finder-fn* q)))
-(defn select [q] (core/select (*finder-fn* q)))
-(defn selected? [q] (core/selected? (*finder-fn* q)))
-(defn send-keys [q s] (core/send-keys (*finder-fn* q) s))
-(defn toggle [q] (core/toggle (*finder-fn* q)))
-(defn all-options [q] (core/all-options (*finder-fn* q)))
-(defn all-selected-options [q] (core/all-selected-options (*finder-fn* q)))
-(defn deselect-option [q attr-val] (core/deselect-option (*finder-fn* q) attr-val))
-(defn deselect-all [q] (core/deselect-all (*finder-fn* q)))
-(defn deselect-by-index [q idx] (core/deselect-by-index (*finder-fn* q) idx))
-(defn deselect-by-text [q text] (core/deselect-by-text (*finder-fn* q) text))
-(defn deselect-by-value [q value] (core/deselect-by-value (*finder-fn* q) value))
-(defn first-selected-option [q] (core/first-selected-option (*finder-fn* q)))
-(defn multiple? [q] (core/multiple? (*finder-fn* q)))
-(defn select-option [q attr-val] (core/select-option (*finder-fn* q) attr-val))
-(defn select-all [q] (core/select-all (*finder-fn* q)))
-(defn select-by-index [q idx] (core/select-by-index (*finder-fn* q) idx))
-(defn select-by-text [q text] (core/select-by-text (*finder-fn* q) text))
-(defn select-by-value [q value] (core/select-by-value (*finder-fn* q) value))
+(defn attribute [q attr] (core/attribute (element q) attr))
+(defn click [q] (core/click (element q)))
+(defn displayed? [q] (core/displayed? (element q)))
+(defn drag-and-drop-by [q x y] (core/drag-and-drop-by (element q) x y))
+(defn drag-and-drop-on [qa qb] (core/drag-and-drop-on (element qa) (element qb)))
+(defn exists? [q] (core/exists? (element q)))
+(defn flash [q] (core/flash (element q)))
+(defn focus [q] (core/focus (element q)))
+(defn html [q] (core/html (element q)))
+(defn location [q] (core/location (element q)))
+(defn location-once-visible [q] (core/location-once-visible (element q)))
+(defn present? [q] (core/present? (element q)))
+(defn size [q] (core/size (element q)))
+(defn rectangle [q] (core/rectangle (element q)))
+(defn intersect? [q & qs] (let [part (partial core/intersect? (element q))]
+                            (apply part (map element qs))))
+(defn tag [q] (core/tag (element q)))
+(defn text [q] (core/text (element q)))
+(defn value [q] (core/value (element q)))
+(defn visible? [q] (core/visible? (element q)))
+(defn xpath [q] (core/xpath (element q)))
+(defn deselect [q] (core/deselect (element q)))
+(defn enabled? [q] (core/enabled? (element q)))
+(defn input-text [q s] (core/input-text (element q) s))
+(defn submit [q] (core/submit (element q)))
+(defn clear [q] (core/clear (element q)))
+(defn select [q] (core/select (element q)))
+(defn selected? [q] (core/selected? (element q)))
+(defn send-keys [q s] (core/send-keys (element q) s))
+(defn toggle [q] (core/toggle (element q)))
+(defn all-options [q] (core/all-options (element q)))
+(defn all-selected-options [q] (core/all-selected-options (element q)))
+(defn deselect-option [q attr-val] (core/deselect-option (element q) attr-val))
+(defn deselect-all [q] (core/deselect-all (element q)))
+(defn deselect-by-index [q idx] (core/deselect-by-index (element q) idx))
+(defn deselect-by-text [q text] (core/deselect-by-text (element q) text))
+(defn deselect-by-value [q value] (core/deselect-by-value (element q) value))
+(defn first-selected-option [q] (core/first-selected-option (element q)))
+(defn multiple? [q] (core/multiple? (element q)))
+(defn select-option [q attr-val] (core/select-option (element q) attr-val))
+(defn select-all [q] (core/select-all (element q)))
+(defn select-by-index [q idx] (core/select-by-index (element q) idx))
+(defn select-by-text [q text] (core/select-by-text (element q) text))
+(defn select-by-value [q value] (core/select-by-value (element q) value))
