af602097d4c8fd9781355f281e8540903e4c6565
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-02-16 16:30:10 -0500

    Continue building out Taxi API test suite, part-way through form field testing

--- test/clj_webdriver/test/taxi.clj
+++ test/clj_webdriver/test/taxi.clj
@@ -2,6 +2,7 @@
   (:use clj-webdriver.taxi
         clj-webdriver.test.config
         [ring.adapter.jetty :only [run-jetty]]
+        [clojure.string :only [lower-case]]
         midje.sweet)
   (:require [clj-webdriver.core :as core]
             [clj-webdriver.test.example-app.core :as web-app]))
@@ -66,6 +67,8 @@
    (attribute "*.first.odd" :class) => "first odd"
    (attribute "li.first.odd" :class) => "first odd"
    (count (elements "a")) => 8)
+
+  ;; Advanced CSS flexible selectors and regex support in clj-webdriver.core
   (go "example-form")
   (facts
    (attribute "*[type='text']" :id) => "first_name"
@@ -74,10 +77,101 @@
    (attribute "input[type='text'][name^='first_']" :id) => "first_name"
    (attribute (find-element {:tag :input, :type "text", :name #"first_"}) :id) => "first_name"
    (attribute (find-element {:tag :input, :type "text", :name #"last_"}) :id) => "last_name"
-   (attribute (find-element {:tag :input, :type "text", :name #"last_"}) :value) => "Smith"
+   (attribute (find-element {:tag :input, :type "text", :name #"last_"}) :value) => "Smith")
+  (go)
+  (facts
+   (text "a[class^='exter']") => "Moustache"
+   (text "a.external[href*='github']") => "Moustache"
+   (text "a[class*='exter'][href*='github']") => "Moustache"
+   (count (elements "*[class*='-item']")) => 3
+   (count (elements "a[class*='-item']")) => 3
+   (count (elements "a[class*='exter'][href*='github']")) => 2)
 
-   ;; Stopped at find-element-should-support-regexes-in-attr-val-map
-   ))
+  ;; Querying "under" elements
+  ;; This is the part that will see more love once #42 is fixed (decouple by-*)
+  ;;
+  ;; You can either use a by-foo function (in clj-webdriver.core), or a map.
+  ;; The map will currently generate a (by-xpath ...) form for you based on the map,
+  ;; but it's not as powerful as the core/find-element map syntax (which handles things
+  ;; like button*, radio, checkbox, etc.).
+  (go)
+  (facts
+   (text (find-element-under "div#content" (core/by-css "a.external"))) => "Moustache"
+   (text (find-element-under "div#content" {:tag :a, :class :external})) => "Moustache"
+   (text (find-element-under "div#content" (core/by-css "a[class*='exter']"))) => "Moustache"
+   (text (find-element-under "div#content" (core/by-css "a[href*='github']"))) => "Moustache"
+   (text (find-element-under "#footer" (core/by-tag :a))) => "home"
+   (count (find-elements-under "#footer" (core/by-tag :a))) => 3
+   (count (find-elements-under "div#content" (core/by-css "a[class*='exter']"))) => 2)
+
+  ;; Exists/visible/present ;;
+  (go)
+  (facts
+   (exists? "a") => truthy
+   (exists? "area") => falsey
+   (exists? "a[href='#pages']") => truthy
+   (visible? "a.external") => truthy
+   (visible? "a[href='#pages']") => falsey
+   (displayed? "a.external") => truthy
+   (displayed? "a[href='#pages']") => falsey
+   (present? "a.external") => truthy
+   (present? "a[href='#pages']") => falsey)
+
+  ;; Element Intersection
+  (go "example-form")
+  (facts
+   (intersect? "#first_name" "#personal-info-wrapper") => truthy
+   (intersect? "#first_name" "#last_name") => falsey)
+
+  ;; XPath generation
+  (go)
+  (fact (xpath "a.external") => "/html/body/div[2]/div/p/a")
+
+  ;; HTML of an element (inner)
+  (go)
+  (fact (html "a.external") => #"href=\"https://github\.com/cgrand/moustache\"")
+
+  ;; Table cell finding
+  (go)
+  (facts
+   (lower-case (tag (find-table-cell "#pages-table" [0 0]))) => "th"
+   (lower-case (tag (find-table-cell "#pages-table" [0 1]))) => "th"
+   (lower-case (tag (find-table-cell "#pages-table" [1 0]))) => "td"
+   (lower-case (tag (find-table-cell "#pages-table" [1 1]))) => "td")
+
+  ;; Table row finding
+  (go)
+  (facts
+   (count (find-table-row "#pages-table" 0)) => 2
+   (lower-case (tag (first (find-table-row "#pages-table" 0)))) => "th"
+   (lower-case (tag (first (find-table-row "#pages-table" 1)))) => "td")
+
+  ;; Form elements ;;
+  (go "example-form")
+
+  ;; Clear
+  (clear "form#example_form input[id^='last_']")
+  (fact (value "form#example_form input[id^='last_']") => empty?)
+
+  ;; Radio buttons
+  (fact (selected? "input[type='radio'][value='male']") => truthy)
+  (select "input[type='radio'][value='female']")
+  (fact (selected? "input[type='radio'][value='female']") => truthy)
+  (select "input[type='radio'][value='male']")
+  (fact (selected? "input[type='radio'][value='male']") => truthy)
+  (fact (selected? "input[type='radio'][value='female']") => falsey)
+
+  ;; Checkboxes
+  (fact (selected? "input[type='checkbox'][name*='clojure']") => falsey)
+  (toggle "input[type='checkbox'][name*='clojure']")
+  (fact (selected? "input[type='checkbox'][name*='clojure']") => truthy)
+  (click "input[type='checkbox'][name*='clojure']")
+  (fact (selected? "input[type='checkbox'][name*='clojure']") => falsey)
+  (select "input[type='checkbox'][name*='clojure']")
+  (fact (selected? "input[type='checkbox'][name*='clojure']") => truthy)
+
+  ;; Text fields
+  )
 
 
 
@@ -90,8 +184,11 @@
    (text "//a[text()='home']") => "home")
   (go)
   (facts
-   (attribute "//*[text()='Moustache']" :href) => "https://github.com/cgrand/moustache"
+   (attribute "//*[text()='Moustache']" :href) => "https://github.com/cgrand/moustache")
 
-   ))
+  ;; XPath wrap strings in double quotes
+  (go)
+  (fact (exists? (find-element {:text "File's Name"})) => truthy)
+  )
 
 (.stop test-server)
