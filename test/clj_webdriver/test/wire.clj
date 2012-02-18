b9761b6ab2d9f39cc431b4e649500e60340a9fb1
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-02-18 16:50:07 -0500

    Fix require statements in test file for Wire protocol support

--- test/clj_webdriver/test/wire.clj
+++ test/clj_webdriver/test/wire.clj
@@ -5,11 +5,11 @@
         [clj-webdriver.test.config :only [test-base-url]]
         [clj-webdriver.test.util :only [start-server]]
         [clj-webdriver.test.common :only [run-common-tests]]
-        [clj-webdriver.remote-server :only [new-remote-session stop]]
-        [clj-webdriver.remote-driver :only [session-id]]
+        [clj-webdriver.remote.server :only [new-remote-session stop]]
+        [clj-webdriver.remote.driver :only [session-id]]
         [clj-webdriver.wire :only [execute]])
   (:require 
-            [clj-webdriver.remote-driver :as rd]))
+            [clj-webdriver.remote.driver :as rd]))
 
 (let [[this-server this-driver] (new-remote-session {:port 3003} {:browser :firefox})]
   (def server this-server)
