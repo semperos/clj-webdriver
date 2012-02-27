e521025339021766226a1e85658b71519d4337f4
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-02-27 09:18:29 -0500

    Make wait-until accept anything, wrapping non-functions in an anonymous function

--- src/clj_webdriver/wait.clj
+++ src/clj_webdriver/wait.clj
@@ -25,7 +25,10 @@
     ([driver pred] (wait-until driver pred 5000 0))
     ([driver pred timeout] (wait-until driver pred timeout 0))
     ([driver pred timeout interval]
-       (let [wait (WebDriverWait. (:webdriver driver) (/ timeout 1000) interval)]
+       (let [wait (WebDriverWait. (:webdriver driver) (/ timeout 1000) interval)
+             pred (if (fn? pred)
+                    pred
+                    (fn [_] pred))]
          (.until wait (proxy [ExpectedCondition] []
                         (apply [d] (pred (init-driver {:webdriver d})))))
          driver))))
