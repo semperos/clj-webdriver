7f16341f7e42635241261ff14b56b64ca6b3058a
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-04-16 22:59:03 -0400

    Add example properties.clj file

---
+++ resources/properties.clj
@@ -0,0 +1,2 @@
+{:FIREFOX_EXTENSION_FIREBUG "/Users/semperos/Library/Application Support/Firefox/Profiles/mektwxbe.default/extensions/firebug@software.joehewitt.com.xpi"
+ :this-file "takes precedence over a Java-style clj_webdriver.properties file."}
