6a4c3244bf4049381ff3beb6dc18e63e81816592
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-04-16 22:48:14 -0400

    Upgrade project.clj to be compatible with Leiningen 2

--- project.clj
+++ project.clj
@@ -1,5 +1,13 @@
-(defproject clj-webdriver "0.6.0-SNAPSHOT"
-  :description "Clojure wrapper around Selenium-WebDriver library"
+(defproject clj-webdriver "0.6.0-SNAPSHOT"   :min-lein-version "2.0.0"
+  :profiles {:dev
+             {:dependencies
+              [[midje "1.3.1"]
+               [criterium "0.2.0"]
+               [codox "0.3.3"]
+               [marginalia "0.3.2"]
+               [ring "1.0.2"]
+               [enlive "1.0.0"]
+               [net.cgrand/moustache "1.0.0"]]}}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.3.0"]
                  [cheshire "2.1.0"]
@@ -8,10 +16,4 @@
                  [com.opera/operadriver "0.10"]
                  [org.clojure/core.cache "0.5.0"]
                  [org.clojure/tools.logging "0.2.3"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [criterium "0.2.0"]
-                     [codox "0.3.3"]
-                     [marginalia "0.3.2"]
-                     [ring "1.0.2"]
-                     [enlive "1.0.0"]
-                     [net.cgrand/moustache "1.0.0"]])
+  :description "Clojure wrapper around Selenium-WebDriver library")
