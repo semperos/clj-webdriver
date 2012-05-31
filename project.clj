ab767d30f85e399b0c31ba2af3147a3b9afe459e
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-05-31 11:17:30 -0400

    Put 'normal' dependencies first in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject clj-webdriver "0.6.0-SNAPSHOT"
   :description "Clojure wrapper around Selenium-WebDriver library"
   :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.3.0"]
+                 [cheshire "2.1.0"]
+                 [org.mortbay.jetty/jetty "6.1.25"]
+                 [org.seleniumhq.selenium/selenium-server "2.21.0"]
+                 ;; [com.opera/operadriver "0.11"]
+                 [org.clojure/core.cache "0.5.0"]
+                 [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev
              {:dependencies
               [[midje "1.3.1"]
@@ -9,12 +17,4 @@
                [marginalia "0.3.2"]
                [ring "1.0.2"]
                [enlive "1.0.0"]
-               [net.cgrand/moustache "1.0.0"]]}}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.3.0"]
-                 [cheshire "2.1.0"]
-                 [org.mortbay.jetty/jetty "6.1.25"]
-                 [org.seleniumhq.selenium/selenium-server "2.21.0"]
-                 ;; [com.opera/operadriver "0.11"]
-                 [org.clojure/core.cache "0.5.0"]
-                 [org.clojure/tools.logging "0.2.3"]])
+               [net.cgrand/moustache "1.0.0"]]}})
