a1a40e647ebfa727db5850d42e91bbc7d2f1206f
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-08-14 18:36:23 -0400

    Add a test suite for running against a manually-started Grid hub

---
+++ test/clj_webdriver/test/remote_manual.clj
@@ -0,0 +1,45 @@
+;; Tests for RemoteWebDriver server and client (driver) code using manually-started Grid hub
+(ns clj-webdriver.test.remote-manual
+  (:use clojure.test
+        [clj-webdriver.core :only [quit to]]
+        [clj-webdriver.test.config :only [test-base-url]]
+        [clj-webdriver.test.util :only [start-server]]
+        [clj-webdriver.test.common :only [run-common-tests]]
+        [clj-webdriver.remote.server :only [new-remote-session stop]])
+  (:require 
+            [clj-webdriver.remote.driver :as rd]))
+
+;; Utilities
+(defn hub-host
+  []
+  (get (System/getenv) "WEBDRIVER_HUB_HOST" "127.0.0.1"))
+
+(defn hub-port
+  []
+  ;; 3001 is default for regular API, so for testing we'll default to 3003
+  (int (get (System/getenv) "WEBDRIVER_HUB_PORT" 3003)))
+
+(let [[this-server this-driver] (new-remote-session {:port (hub-port) :host (hub-host)}
+                                                    {:browser :firefox}
+                                                    false)]
+  (def server this-server)
+  (def driver this-driver))
+
+;; Fixtures
+(defn reset-browser-fixture
+  [f]
+  (to driver test-base-url)
+  (f))
+
+(defn quit-fixture
+  [f]
+  (f)
+  (quit driver)
+  (stop server))
+
+(use-fixtures :once start-server quit-fixture)
+(use-fixtures :each reset-browser-fixture)
+
+;; RUN TESTS HERE
+(deftest test-suite-with-remote-driver-attached-to-manually-started
+  (run-common-tests driver))
