4578e2d9fe40d58747cf99990c25ebd4b38502bf
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-09-06 20:38:09 -0400

    Remove commented code

--- src/clj_webdriver/ext/remote/RemoteWebDriverExt.clj
+++ src/clj_webdriver/ext/remote/RemoteWebDriverExt.clj
@@ -2,16 +2,12 @@
   clj-webdriver.ext.remote.RemoteWebDriverExt
   (:import [org.openqa.selenium.remote DriverCommand RemoteWebDriver])
   (:gen-class
-   ;; :name demo.interop.Instance
    :main false
    :extends org.openqa.selenium.remote.RemoteWebDriver
    :implements [org.openqa.selenium.TakesScreenshot]))
 
 (defn -getScreenshotAs
   [this target]
-    ;;   String base64 = (String) execute(DriverCommand.SCREENSHOT).getValue();
-    ;; // ... and convert it.
-  ;; return target.convertFromBase64Png(base64);
   (let [base64 (->> DriverCommand/SCREENSHOT
                     (. this execute)
                     .getValue
