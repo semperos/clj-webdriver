178a96645bcebed14ebab2fb74c805a3972d36fb
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-10-05 20:50:04 -0400

    Remove capabilities fn from IRemoteWebDriver protocol, as it's now a field in the Driver record

--- src/clj_webdriver/remote/driver.clj
+++ src/clj_webdriver/remote/driver.clj
@@ -1,4 +1,5 @@
 (ns clj-webdriver.remote.driver
+  (:require clj-webdriver.driver)
   (:import [org.openqa.selenium.remote
             DesiredCapabilities
             RemoteWebDriver]
@@ -6,10 +7,7 @@
 
 (defprotocol IRemoteWebDriver
   "RemoteWebDriver-specific functionality"
-  (capabilities [driver] "Get capabilities of running `driver`")
   (command-executor [driver] "Get the CommandExecutor instance attached to this `driver`")
   (command-executor! [driver executor] "Set the CommandExecutor of the given `driver`")
   (session-id [driver] "Get the session id for the given `driver`")
   (session-id! [driver new-id] "Set the session id for the given `driver`"))
-
-
