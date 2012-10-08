4127e659873a86ff5a99bd74382c4057973ed552
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-10-08 10:42:20 -0400

    Return definitions of IRemoteWebDriver to clj-webdriver.remote.driver ns, was too delete-happy a few commits back

--- src/clj_webdriver/remote/driver.clj
+++ src/clj_webdriver/remote/driver.clj
@@ -11,3 +11,20 @@
   (command-executor! [driver executor] "Set the CommandExecutor of the given `driver`")
   (session-id [driver] "Get the session id for the given `driver`")
   (session-id! [driver new-id] "Set the session id for the given `driver`"))
+
+(extend-type Driver
+  IRemoteWebDriver
+  (capabilities [driver]
+    (.getCapabilities (:webdriver driver)))
+  
+  (command-executor [driver]
+    (.getCommandExecutor (:webdriver driver)))
+  
+  (command-executor! [driver executor]
+    (.setCommandExecutor (:webdriver driver) executor))
+  
+  (session-id [driver]
+    (str (.getSessionId (:webdriver driver))))
+  
+  (session-id! [driver new-id]
+    (.setSessionId (:webdriver driver) new-id)))
