2effaf6b68c7bab1348785c75364dcb6c4f3c93f
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-11-06 09:40:18 -0500

    Remove templating from Taxi API tests.
    Any further work that will be accomplished in this area will be
    focused in the 'taxi-templated-param' branch. While some tests
    took to templating nicely, others exhibited non-deterministic
    behavior related directly to having multiple browsers running
    in the same process simultaneously.

--- test/clj_webdriver/test/taxi.clj
+++ test/clj_webdriver/test/taxi.clj
@@ -8,101 +8,89 @@
             [clj-webdriver.test.example-app.core :as web-app])
   (:import [org.openqa.selenium TimeoutException NoAlertPresentException]))
 
-;; Alternate driver (not used with Taxi's set-driver!)
-(def alt-driver (atom nil))
-
 (defn start-browser-fixture
   [f]
   (set-driver! {:browser :firefox})
-  (reset! alt-driver (core/new-driver {:browser :firefox}))
   (f))
 
 (defn reset-browser-fixture
   [f]
   (to test-base-url)
-  (to @alt-driver test-base-url)
   (f))
 
 (defn quit-browser-fixture
   [f]
   (f)
-  (quit)
-  (quit @alt-driver))
+  (quit))
 
 (use-fixtures :once start-server start-browser-fixture quit-browser-fixture)
 (use-fixtures :each reset-browser-fixture)
 
 ;; RUN TESTS HERE
-(deftest-template-param
-  test-browser-basics @alt-driver
+(deftest test-browser-basics
   (is (= (class *driver*) clj_webdriver.driver.Driver))
-  (is (= (current-url __) test-base-url))
-  (is (= (title __) "Ministache"))
-  (is (re-find #"(?i)html>" (page-source __))))
+  (is (= (current-url) test-base-url))
+  (is (= (title) "Ministache"))
+  (is (re-find #"(?i)html>" (page-source))))
 
-(deftest-template-param
-  back-forward-should-traverse-browser-history @alt-driver
-  (click __ (find-element __ {:tag :a, :text "example form"}))
-  (is (= (current-url __) (str test-base-url "example-form")))
-  (back __)
-  (is (= (current-url __) test-base-url))
-  (forward __)
-  (is (= (current-url __) (str test-base-url "example-form")))
-  (click __ (find-element __ {:tag :a, :text "clojure"}))
-  (back __ 2)
-  (is (= (current-url __) (str test-base-url)))
-  (forward __ 2)
-  (is (= (current-url __) (str test-base-url "clojure"))))
+(deftest back-forward-should-traverse-browser-history
+  (click (find-element {:tag :a, :text "example form"}))
+  (is (= (current-url) (str test-base-url "example-form")))
+  (back)
+  (is (= (current-url) test-base-url))
+  (forward)
+  (is (= (current-url) (str test-base-url "example-form")))
+  (click (find-element {:tag :a, :text "clojure"}))
+  (back 2)
+  (is (= (current-url) (str test-base-url)))
+  (forward 2)
+  (is (= (current-url) (str test-base-url "clojure"))))
 
-(deftest-template-param
-  to-should-open-given-url-in-browser @alt-driver
-  (to __ (str test-base-url "example-form"))
-  (is (= (current-url __) (str test-base-url "example-form")))
-  (is (= (title __) "Ministache")))
+(deftest to-should-open-given-url-in-browser
+  (to (str test-base-url "example-form"))
+  (is (= (current-url) (str test-base-url "example-form")))
+  (is (= (title) "Ministache")))
 
-(deftest-template-param
-  test-cookie-handling @alt-driver
-  (add-cookie __ {:name "my_cookie" :value "my_cookie_value"})
-  (is (> (count (cookies __)) 0))
-  (is (= (:value (cookie __ "my_cookie")) "my_cookie_value"))
-  (delete-cookie __ "my_cookie")
-  (is (not (some (fn [c] (= (:name c) "my_cookie")) (cookies __))))
-  (delete-all-cookies __)
-  (is (zero? (count (cookies __)))))
+(deftest test-cookie-handling
+  (add-cookie {:name "my_cookie" :value "my_cookie_value"})
+  (is (> (count (cookies)) 0))
+  (is (= (:value (cookie "my_cookie")) "my_cookie_value"))
+  (delete-cookie "my_cookie")
+  (is (not (some (fn [c] (= (:name c) "my_cookie")) (cookies))))
+  (delete-all-cookies)
+  (is (zero? (count (cookies)))))
 
-(deftest-template-param
-  test-finding-elements-with-CSS @alt-driver
-  (click __ (find-element __ {:tag :a, :text "example form"}))
-  (is (= (attribute __ "#first_name" :id) "first_name"))
-  (is (= (attribute __ "*[type='text']" :id) "first_name"))
-  (is (= (attribute __ "input[name='first_name']" :id) "first_name"))
-  (is (= (attribute __ "input[type='text'][name='first_name']" :id) "first_name"))
-  (is (= (attribute __ "input[type='text'][name^='first_']" :id) "first_name"))
-  (is (= (text __ "a") "home"))
-  (is (= (text __ "a.menu-item") "home"))
-  (is (= (attribute __ {:tag "a", :class "menu-item"} :class) "menu-item"))
-  (is (= (text __ "#footer a.menu-item") "home"))
-  (is (= (attribute __ "option[value*='cial_']" :value) "social_media"))
-  (is (= (attribute __ "option[value^='social_']" :value) "social_media"))
-  (is (= (attribute __ "option[value$='_media']" :value) "social_media"))
-  (is (= (attribute __ "option[value]" :value) "france"))
-  (back __) ;; on starting page
-  (is (= (attribute __ "*.first.odd" :class) "first odd"))
-  (is (= (tag __ {:class "external"}) "a"))
-  (is (= (text __ (nth (elements __ "a") 1)) "Moustache"))
-  (is (= (text __ "*.external") "Moustache"))
-  (is (= (attribute __ "*.first.odd" :class) "first odd"))
-  (is (= (attribute __ "li.first.odd" :class) "first odd"))
-  (is (= (count (elements __ "a")) 10))
-  (is (= (text __ "a[class^='exter']") "Moustache"))
-  (is (= (text __ "a.external[href*='github']") "Moustache"))
-  (is (= (text __ "a[class*='exter'][href*='github']") "Moustache"))
-  (is (= (count (elements __ "*[class*='-item']")) 5))
-  (is (= (count (elements __ "a[class*='-item']")) 5))
-  (is (= (count (elements __ "a[class*='exter'][href*='github']")) 2)))
+(deftest test-finding-elements-with-CSS
+  (click (find-element {:tag :a, :text "example form"}))
+  (is (= (attribute "#first_name" :id) "first_name"))
+  (is (= (attribute "*[type='text']" :id) "first_name"))
+  (is (= (attribute "input[name='first_name']" :id) "first_name"))
+  (is (= (attribute "input[type='text'][name='first_name']" :id) "first_name"))
+  (is (= (attribute "input[type='text'][name^='first_']" :id) "first_name"))
+  (is (= (text "a") "home"))
+  (is (= (text "a.menu-item") "home"))
+  (is (= (attribute {:tag "a", :class "menu-item"} :class) "menu-item"))
+  (is (= (text "#footer a.menu-item") "home"))
+  (is (= (attribute "option[value*='cial_']" :value) "social_media"))
+  (is (= (attribute "option[value^='social_']" :value) "social_media"))
+  (is (= (attribute "option[value$='_media']" :value) "social_media"))
+  (is (= (attribute "option[value]" :value) "france"))
+  (back) ;; on starting page
+  (is (= (attribute "*.first.odd" :class) "first odd"))
+  (is (= (tag {:class "external"}) "a"))
+  (is (= (text (nth (elements "a") 1)) "Moustache"))
+  (is (= (text "*.external") "Moustache"))
+  (is (= (attribute "*.first.odd" :class) "first odd"))
+  (is (= (attribute "li.first.odd" :class) "first odd"))
+  (is (= (count (elements "a")) 10))
+  (is (= (text "a[class^='exter']") "Moustache"))
+  (is (= (text "a.external[href*='github']") "Moustache"))
+  (is (= (text "a[class*='exter'][href*='github']") "Moustache"))
+  (is (= (count (elements "*[class*='-item']")) 5))
+  (is (= (count (elements "a[class*='-item']")) 5))
+  (is (= (count (elements "a[class*='exter'][href*='github']")) 2)))
 
-(deftest-template-param
-  test-querying-under-elements @alt-driver
+(deftest test-querying-under-elements
   (is (= (text (find-element-under "div#content" (core/by-css "a.external"))) "Moustache"))
   (is (= (text (find-element-under "div#content" {:css "a.external"})) "Moustache"))
   (is (= (text (find-element-under "div#content" {:tag :a, :class "external"})) "Moustache"))
@@ -112,20 +100,18 @@
   (is (= (count (find-elements-under "#footer" {:tag :a})) 5))
   (is (= (count (find-elements-under "div#content" {:css "a[class*='exter']"})) 2)))
 
-(deftest-template-param
-  text-exists-visible-present @alt-driver
-  (is (exists? __ "a"))
-  (is (not (exists? __ "area")))
-  (is (exists? __ "a[href='#pages']"))
-  (is (visible? __ "a.external"))
-  (is (not (visible? __ "a[href='#pages']")))
-  (is (displayed? __ "a.external"))
-  (is (not (displayed? __ "a[href='#pages']")))
-  (is (present? __ "a.external"))
-  (is (not (present? __ "a[href='#pages']"))))
+(deftest text-exists-visible-present
+  (is (exists? "a"))
+  (is (not (exists? "area")))
+  (is (exists? "a[href='#pages']"))
+  (is (visible? "a.external"))
+  (is (not (visible? "a[href='#pages']")))
+  (is (displayed? "a.external"))
+  (is (not (displayed? "a[href='#pages']")))
+  (is (present? "a.external"))
+  (is (not (present? "a[href='#pages']"))))
 
-(deftest-template-param
-  test-drag-and-drop @alt-driver
+(deftest test-drag-and-drop
   (click (find-element {:tag :a, :text "javascript playground"}))
   (is (present? "#draggable"))
   (let [el-to-drag (element {:id "draggable"})
@@ -138,8 +124,7 @@
     (is (= x-diff 20))
     (is (= y-diff 20))))
 
-(deftest-template-param
-  drag-and-drop-on-elements-should-work @alt-driver
+(deftest drag-and-drop-on-elements-should-work
   (click (find-element {:tag :a, :text "javascript playground"}))
   ;; Just check to make sure this page still has the element we expect,
   ;; since it's an external site
@@ -155,22 +140,18 @@
             (not= o-y n-y)))
     (is (re-find #"ui-state-highlight" (attribute droppable :class)))))
 
-(deftest-template-param
-  test-element-intersection @alt-driver
+(deftest test-element-intersection
   (click (find-element {:tag :a, :text "example form"}))
   (is (intersects? "#first_name" "#personal-info-wrapper"))
   (is (not (intersects? "#first_name" "#last_name"))))
 
-(deftest-template-param
-  test-xpath-output @alt-driver
+(deftest test-xpath-output
   (is (= (xpath "a.external") "/html/body/div[2]/div/p/a")))
 
-(deftest-template-param
-  test-html-output @alt-driver
+(deftest test-html-output
   (is (re-find #"href=\"https://github\.com/cgrand/moustache\"" (html "a.external"))))
 
-(deftest-template-param
-  test-table-finding @alt-driver
+(deftest test-table-finding
   (is (= (current-url) test-base-url))
   (is (exists? "#pages-table"))
   (is (= (lower-case (tag (find-table-cell "#pages-table" [0 0]))) "th"))
@@ -181,8 +162,7 @@
   (is (= (lower-case (tag (first (find-table-row (element "#pages-table") 0)))) "th"))
   (is (= (lower-case (tag (first (find-table-row (element"#pages-table") 1)))) "td")))
 
-(deftest-template-param
-  form-elements @alt-driver
+(deftest form-elements
   (click (find-element {:tag :a :text, "example form"}))
   ;; Clear
   (clear "form#example_form input[id^='last_']")
@@ -261,8 +241,7 @@
     (deselect-all q)
     (is (zero? (count (selected-options q))))))
 
-(deftest-template-param
-  test-quickfill @alt-driver
+(deftest test-quickfill
   (click (find-element {:tag :a :text, "example form"}))
   (is (= (count (quick-fill
                             {"#first_name" clear}
@@ -298,8 +277,7 @@
                                {"input[type='radio'][value='female']" click}
                                {"select#countries" #(select-by-value % "france")}))))
 
-(deftest-template-param
-  test-window-handling @alt-driver
+(deftest test-window-handling
   (is (= (count (windows)) 1))
   (is (= (:title (window)) "Ministache"))
   (click "a[target='_blank'][href*='clojure']")
@@ -313,8 +291,7 @@
   (close)
   (is (= (:url (window)) test-base-url)))
 
-(deftest-template-param
-  test-waiting-until @alt-driver
+(deftest test-waiting-until
   (is (= (title) "Ministache"))
   (execute-script "setTimeout(function () { window.document.title = \"asdf\"}, 3000)")
   (wait-until #(= (title) "asdf") 5000 0)
@@ -331,8 +308,7 @@
   ;;                     (wait-until #(= (title) "test") 1000 0)))))
   )
 
-(deftest-template-param
-  test-implicit-wait @alt-driver
+(deftest test-implicit-wait
   (implicit-wait 3000)
   (execute-script "setTimeout(function () { window.document.body.innerHTML = \"<div id='test'>hi!</div>\"}, 1000)")
   (is (= (attribute "#test" :id) "test")))
@@ -342,7 +318,7 @@
 ;; is different at page start. Disabling for now, as this usage is not
 ;; as cross-browser compatible or reliable.
 ;;
-;; (deftest-template-param test-frames-by-index
+;; (deftest test-frames-by-index
 ;;   (to "http://selenium.googlecode.com/svn/trunk/docs/api/java/index.html")
 ;;   (is (= (count (elements "frame")) 3))
 ;;   (switch-to-frame 0)
@@ -354,8 +330,7 @@
 ;;   (switch-to-frame 2)
 ;;   (is (exclusive-between (count (elements "a")) 50 100)))
 
-(deftest-template-param
-  test-frames-by-element @alt-driver
+(deftest test-frames-by-element
   (to "http://selenium.googlecode.com/svn/trunk/docs/api/java/index.html")
   (is (= (count (elements "frame")) 3))
   (switch-to-frame "frame[name='packageListFrame']")
@@ -367,12 +342,10 @@
   (switch-to-frame "frame[name='classFrame']")
   (is (exclusive-between (count (elements "a")) 50 100)))
 
-(deftest-template-param
-  test-flash @alt-driver
+(deftest test-flash
   (is (= (attribute (flash "a.external") :class) "external")))
 
-(deftest-template-param
-  test-using-xpath-instead-of-css @alt-driver
+(deftest test-using-xpath-instead-of-css
   (set-finder! xpath-finder)
   (click (find-element {:tag :a :text, "example form"}))
   (is (= (text {:tag :a, :text "home"}) "home"))
@@ -382,11 +355,10 @@
   (is (= (attribute "//*[text()='Moustache']" :href) "https://github.com/cgrand/moustache"))
   (is (exists? (find-element {:text "File's Name"}))))
 
-(deftest-template-param
-  test-alert-dialog-handling @alt-driver
+(deftest test-alert-dialog-handling
   (click (find-element {:tag :a, :text "example form"}))
   (let [act (fn [] (click "button"))]
-    (act)    
+    (act)
     (is (alert-obj) "No alert dialog could be located")
     (accept)
     (is (thrown? NoAlertPresentException
@@ -398,8 +370,7 @@
     (is (thrown? NoAlertPresentException
                  (alert-obj)))))
 
-(deftest-template-param
-  test-window-size @alt-driver
+(deftest test-window-size
   (let [orig-size (window-size)
         small {:width 500 :height 400}
         large {:width 1024 :height 800}]
@@ -410,8 +381,7 @@
     (window-resize orig-size)
     (is (= (window-size) orig-size))))
 
-(deftest-template-param
-  test-window-resize-with-one-dimension @alt-driver
+(deftest test-window-resize-with-one-dimension
   (let [orig-size (window-size)
         small {:height 400}
         large {:width 1024}]
@@ -422,8 +392,7 @@
     (window-resize large)
     (is (= (:height (window-size)) (:height orig-size)))))
 
-(deftest-template-param
-  test-window-position @alt-driver
+(deftest test-window-position
   (let [origin (window-position)
         new-position {:x 100 :y 245}]
     (window-reposition new-position)
@@ -431,8 +400,7 @@
     (window-reposition origin)
     (is (= (window-position) origin))))
 
-(deftest-template-param
-  test-window-reposition-with-one-coordinate @alt-driver
+(deftest test-window-reposition-with-one-coordinate
   (let [origin (window-position)
         position-y {:y 245}
         position-x {:x 100}]
@@ -443,8 +411,7 @@
     (window-reposition position-x)
     (is (= (:y (window-position)) (:y origin)))))
 
-(deftest-template-param
-  test-window-maximizing @alt-driver
+(deftest test-window-maximizing
   (let [orig-size (window-size (window-resize {:width 300 :height 300}))
         max-size (window-size (window-maximize))]
     (is (> (:width max-size) (:width orig-size)))
