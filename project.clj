3c486b7ba2014ec7900d92918f032850a322149c
Tom Marble <tmarble@info9.net>
2015-05-13 10:45:19 -0500

    updated dependencies for v0.7.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.info9/clj-webdriver "0.7.3"
+(defproject net.info9/clj-webdriver "0.7.4"
   :description "Clojure API for Selenium-WebDriver"
   :url "https://github.com/semperos/clj-webdriver"
   :license {:name "Eclipse Public License"
@@ -7,20 +7,20 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.cache "0.6.4"]
                  [org.clojure/tools.logging "0.3.1"]
-                 ;; Exclude these, giving preference to Selenium-WebDriver's
-                 ;; dependence on them.
-                 [clj-http "1.1.0" :exclusions
-                  [org.apache.httpcomponents/httpclient
-                   org.apache.httpcomponents/httpcore
-                   org.apache.httpcomponents/httpmime]]
+                 [org.clojure/tools.namespace "0.2.10"]
+                 [clj-http "1.1.2"]
                  [cheshire "5.4.0"]
                  [org.mortbay.jetty/servlet-api-2.5 "6.1.14"]
-                 [org.eclipse.jetty/jetty-server "8.1.16.v20140903"]
-                 [org.eclipse.jetty/jetty-webapp "8.1.16.v20140903"]
-                 [org.eclipse.jetty/jetty-websocket "8.1.16.v20140903"]
-                 [org.seleniumhq.selenium/selenium-server "2.45.0"]
-                 [org.seleniumhq.selenium/selenium-java "2.45.0"]
-                 [org.seleniumhq.selenium/selenium-remote-driver "2.45.0"]
+                 [org.eclipse.jetty/jetty-server "8.1.17.v20150415"]
+                 [org.eclipse.jetty/jetty-webapp "8.1.17.v20150415"]
+                 [org.eclipse.jetty/jetty-websocket "8.1.17.v20150415"]
+                 [com.google.code.gson/gson "2.2.4"]
+                 [org.seleniumhq.selenium/selenium-server "2.45.0"
+                  :exclusions [commons-codec com.google.code.gson/gson]]
+                 [org.seleniumhq.selenium/selenium-java "2.45.0"
+                  :exclusions [commons-codec com.google.code.gson/gson]]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.45.0"
+                  :exclusions [com.google.code.gson/gson]]
                  [com.github.detro/phantomjsdriver "1.2.0" :exclusions
                   [org.seleniumhq.selenium/selenium-java
                    org.seleniumhq.selenium/selenium-server
@@ -28,12 +28,16 @@
   :profiles {:dev
              {:dependencies
               [[criterium "0.4.3"]
-               [codox "0.8.11"]
+               [codox "0.8.12"]
                [clj-time "0.9.0"]
-               [marginalia "0.8.0"]
-               [ring "1.3.2" :exclusions
+               [marginalia "0.8.0"
+                :exclusions
                 [org.clojure/tools.namespace
-                 org.clojure/java.classpath]]
+                 org.clojure/java.classpath]
+                ]
+               [ring "1.3.2"
+                :exclusions
+                [org.clojure/tools.namespace]]
                [ring-http-basic-auth "0.0.2"]
                [enlive "1.1.5"]
                [net.cgrand/moustache "1.1.0" :exclusions
