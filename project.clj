efb34663a673059ab0b016277fb3063f74734721
Tom Marble <tmarble@info9.net>
2015-08-03 18:43:56 -0500

    updated dependencies for v0.7.5

--- project.clj
+++ project.clj
@@ -1,26 +1,23 @@
-(defproject net.info9/clj-webdriver "0.7.4"
+(defproject net.info9/clj-webdriver "0.7.5"
   :description "Clojure API for Selenium-WebDriver"
   :url "https://github.com/semperos/clj-webdriver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.cache "0.6.4"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.namespace "0.2.10"]
-                 [clj-http "1.1.2"]
-                 [cheshire "5.4.0"]
+                 [clj-http "2.0.0"]
+                 [cheshire "5.5.0"]
                  [org.mortbay.jetty/servlet-api-2.5 "6.1.14"]
-                 [org.eclipse.jetty/jetty-server "8.1.17.v20150415"]
-                 [org.eclipse.jetty/jetty-webapp "8.1.17.v20150415"]
-                 [org.eclipse.jetty/jetty-websocket "8.1.17.v20150415"]
-                 [com.google.code.gson/gson "2.2.4"]
-                 [org.seleniumhq.selenium/selenium-server "2.45.0"
-                  :exclusions [commons-codec com.google.code.gson/gson]]
-                 [org.seleniumhq.selenium/selenium-java "2.45.0"
-                  :exclusions [commons-codec com.google.code.gson/gson]]
-                 [org.seleniumhq.selenium/selenium-remote-driver "2.45.0"
-                  :exclusions [com.google.code.gson/gson]]
+                 [org.eclipse.jetty.websocket/websocket-client "9.2.10.v20150310"]
+                 [com.google.code.gson/gson "2.3.1"]
+                 [org.seleniumhq.selenium/selenium-server "2.47.1"
+                  :exclusions [org.eclipse.jetty.websocket/websocket-client]]
+                 [org.seleniumhq.selenium/selenium-java "2.47.1"
+                  :exclusions [org.eclipse.jetty.websocket/websocket-client]]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.47.1"]
                  [com.github.detro/phantomjsdriver "1.2.0" :exclusions
                   [org.seleniumhq.selenium/selenium-java
                    org.seleniumhq.selenium/selenium-server
@@ -28,18 +25,15 @@
   :profiles {:dev
              {:dependencies
               [[criterium "0.4.3"]
-               [codox "0.8.12"]
-               [clj-time "0.9.0"]
+               [codox "0.8.13"]
+               [clj-time "0.10.0"]
+               [org.clojure/clojurescript "1.7.28"]
                [marginalia "0.8.0"
                 :exclusions
-                [org.clojure/tools.namespace
-                 org.clojure/java.classpath]
-                ]
-               [ring "1.3.2"
-                :exclusions
-                [org.clojure/tools.namespace]]
+                [org.clojure/clojurescript]]
+               [ring "1.4.0"]
                [ring-http-basic-auth "0.0.2"]
-               [enlive "1.1.5"]
+               [enlive "1.1.6"]
                [net.cgrand/moustache "1.1.0" :exclusions
                 [org.clojure/clojure
                  ring/ring-core
