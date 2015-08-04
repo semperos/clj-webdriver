3b00f90fc98634a7f618654b4c0419a771df2084
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2015-08-04 14:49:00 -0400

    Get PhantomJS tests working

--- test/clj_webdriver/phantomjs_test.clj
+++ test/clj_webdriver/phantomjs_test.clj
@@ -1,36 +1,31 @@
-(ns clj-webdriver.test.phantomjs
+(ns clj-webdriver.phantomjs-test
   (:require [clojure.tools.logging :as log]
             [clojure.test :refer :all]
             [clj-webdriver.core :refer [start new-driver to quit]]
             [clj-webdriver.driver :refer [init-driver]]
-            [clj-webdriver.test.common :refer [run-common-tests run-phantomjs-tests]]
+            [clj-webdriver.test.common :refer [defcommontests]]
             [clj-webdriver.test.helpers :refer [base-url start-system! stop-system!]])
   (:import org.openqa.selenium.remote.DesiredCapabilities))
 
-;; Driver definitions
-(log/debug "WARNING: The PhantomJS driver requires a separate download. See https://github.com/detro/ghostdriver for more information if PhantomJS fails to start.")
+;; The PhantomJS driver requires a separate download. See https://github.com/detro/ghostdriver for more information if PhantomJS fails to start.
 (def phantomjs-driver (atom nil))
 
 ;; Fixtures
-(defn start-browser-fixture
+(defn restart-browser
   [f]
-  (reset! phantomjs-driver
-          (new-driver {:browser :phantomjs}))
-  (f))
-
-(defn reset-browser-fixture
-  [f]
+  (when-not @phantomjs-driver
+    (reset! phantomjs-driver
+            (new-driver {:browser :phantomjs})))
   (to @phantomjs-driver base-url)
   (f))
 
-(defn quit-browser-fixture
+(defn quit-browser
   [f]
   (f)
   (quit @phantomjs-driver))
 
-(use-fixtures :once start-system! stop-system! start-browser-fixture quit-browser-fixture)
-(use-fixtures :each reset-browser-fixture)
+(use-fixtures :once start-system! stop-system! quit-browser)
+(use-fixtures :each restart-browser)
 
 ;; RUN TESTS HERE
-(deftest test-common-features-for-phantomjs
-  (run-phantomjs-tests @phantomjs-driver))
+(defcommontests "test-" @phantomjs-driver)
