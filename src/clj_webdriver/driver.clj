95555771eeab358c397c4e4a28a45d9c7a0a3aac
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2015-08-13 21:46:45 -0400

    Change Driver to deftype, new structure
    For now contains WebDriver (like before) and `last` which will contain
    the return value of the last operation if it returned anything other
    than the driver itself.

--- src/clj_webdriver/driver.clj
+++ src/clj_webdriver/driver.clj
@@ -1,26 +1,21 @@
 (ns clj-webdriver.driver
-  (:import org.openqa.selenium.interactions.Actions))
+  (:require [clojure.core.protocols :as p])
+  (:import java.util.Iterator
+           org.openqa.selenium.interactions.Actions))
 
-(defrecord Driver [webdriver capabilities actions])
+(deftype Driver [webdriver last]
+  Iterable
+  (iterator [driver]
+    (let [consumed? (atom false)]
+      (reify Iterator
+        (hasNext [_] (not @consumed?))
+        (next [driver]
+          (if @consumed?
+            nil
+            (do
+              (swap! consumed? not)
+              driver))))))
 
-(defn init-driver
-  "Constructor for Driver records. Accepts either an existing WebDriver instance, or a `driver-spec` map with the following keys:
-
-   webdriver - WebDriver instance"
-  ([] (init-driver {}))
-  ([driver-spec]
-     (let [wd-class (Class/forName "org.openqa.selenium.WebDriver")
-           uppers (supers (.getClass driver-spec))]
-       (if (some #{wd-class} uppers)
-         (Driver. driver-spec
-                  nil
-                  (Actions. driver-spec))
-         (let [{:keys [webdriver capabilities]} driver-spec]
-           (Driver. webdriver
-                    capabilities
-                    (Actions. webdriver)))))))
-
-(defn driver?
-  "Function to check class of a Driver, to prevent needing to import it"
-  [driver]
-  (= (class driver) Driver))
+  p/CollReduce
+  (coll-reduce [driver f] (f driver))
+  (coll-reduce [driver f val] (f driver val)))
