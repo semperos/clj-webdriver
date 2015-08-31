390dfe47b1c04d979e3ae34bd61a49c514884cb5
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2015-08-31 16:38:09 -0400

    More tests from common, realize verbosity
    The verbosity of getting values out of a monadic computation is apparent
    with these tests. Need to consider further monad that will allow
    integration of test assertions as a first-class concept within the
    execution of the monad itself, instead of requiring elaborate "export"
    features like identity-map.

--- test/webdriver/monad_test.clj
+++ test/webdriver/monad_test.clj
@@ -29,9 +29,7 @@
   (let [test (drive current-url <- (current-url)
                     title <- (title)
                     page-source <- (page-source)
-                    {:current-url current-url
-                     :title title
-                     :page-source page-source})
+                    (identity-map current-url title page-source))
         [results driver] (test @driv)]
     (is (map? driver))
     (is (instance? WebDriver (:webdriver driver)))
@@ -51,11 +49,61 @@
                     url-orig <- (current-url)
                     (forward)
                     url-form2 <- (current-url)
-                    {:url-form url-form
-                     :url-orig url-orig
-                     :url-form2 url-form2})]
+                    (identity-map url-form url-orig url-form2))]
     (let [result (test @driv)]
       (is (= {:url-form (str *base-url* "example-form")
               :url-orig *base-url*
               :url-form2 (str *base-url* "example-form")}
              (first result))))))
+
+(deftest to-should-open-given-url-in-browser
+  (let [test (drive
+              (to (str *base-url* "example-form"))
+              url <- (current-url)
+              title <- (title)
+              [url title])
+        [[url title]] (test @driv)]
+    (is (= (str *base-url* "example-form") url))
+    (is (= "Ministache" title))))
+
+(deftest should-be-able-to-find-element-bys-using-low-level-by-wrappers
+  ;; Get to example form page
+  ((drive
+    (click {:tag :a, :text "example form"})
+    (wait-until (drive
+                 el <- (find-element-by (by-id "first_name"))
+                 el))
+    :done) @driv)
+  ;; Exercise both `attribute` and different `by-*` types
+  (let [first-name (drive
+                    id <- (attribute (by-name "first_name") :id)
+                    name <- (attribute (by-id "first_name") :name)
+                    (identity-map id name))
+        [results] (first-name @driv)]
+    ;; TODO Bad test if both values are same.
+    (is (= "first_name" (:id results)))
+    (is (= "first_name" (:name results))))
+  (let [home (drive
+              link-text-full <- (text (by-link-text "home"))
+              link-text-xpath <- (text (by-xpath "//a[text()='home']"))
+              link-text-tag <- (text (by-tag "a"))
+              link-text-class <- (text (by-class-name "menu-item"))
+              link-text-css <- (text (by-css-selector "#footer a.menu-item"))
+              [link-text-full link-text-xpath link-text-tag link-text-class link-text-css])
+        [results] (home @driv)]
+    (= (take 5 (repeat "home"))
+       results)))
+;; example-form-text <- (text (by-partial-link-text "example"))
+;;   (is (= "example form"
+;;          (text (find-element-by driver (by-partial-link-text "example")))))
+;;   (is (= "social_media"
+;;          (attribute (find-element-by driver (by-attr-contains :option :value "cial_")) :value)))
+;;   (is (= "social_media"
+;;          (attribute (find-element-by driver (by-attr-starts :option :value "social_")) :value)))
+;;   (is (= "social_media"
+;;          (attribute (find-element-by driver (by-attr-ends :option :value "_media")) :value)))
+;;   (is (= "france"
+;;          (attribute (find-element-by driver (by-has-attr :option :value)) :value)))
+;;   (to driver *base-url*)
+;;   (is (= "first odd"
+;;          (attribute (find-element-by driver (by-class-name "first odd")) :class)))
